db752370208a025750b186a07a274d502fba513a
unknown <satulurihareeshbabu@gmail.com>
2018-05-17 00:49:48 -0700

    after commit made changes.

--- singlepage/src/app/app.module.ts
+++ singlepage/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from  '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -58,7 +58,7 @@ const routes=[
     FiltercoursePipe
   ],
   imports: [
-    BrowserModule,RouterModule.forRoot(routes),HttpClientModule,FormsModule
+    BrowserModule,RouterModule.forRoot(routes),HttpClientModule,FormsModule,ReactiveFormsModule
   ],
   providers: [CourseService,ReviewService,EnquiryService],
   bootstrap: [AppComponent]
